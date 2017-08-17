0568beda42b5d489980aa6669656d88141965987
ederAguirre <edjoo1113@gmail.com>
2017-08-17 00:48:08 -0500

    Cifrado AES
    Hice el cifrado AES, ya puede elegir archivos, pero aún no se como
    almacenar el archivo en una variable, o leerlo desde alguna función

---
+++ TT-2016-B045/Codificacion/AES.py
@@ -0,0 +1,41 @@
+################################  Elaborado Por: Eder J. Aguirre Cruz
+
+
+from Crypto.Cipher import AES
+from Crypto import Random
+from tkinter.filedialog import *
+
+
+
+########################################################### Comienza el cifrado AES ###################################################################
+
+print("############################ CIFRADOR AES ###############################")
+print()
+print()
+
+print("############################ Comienza el Cifrado ###############################")
+keyC= input("Introduce la llave (16 Caracteres): ")
+iv = Random.new().read(AES.block_size)   ##Creación de Vector de Inicialización
+obj = AES.new(keyC, AES.MODE_CBC, iv)    ##Creando Nuevo Objeto para cifrado AES
+#filename = askopenfilename()             # ##Dialogo para elegir archivo, y almacena ruta del archivo
+f = asksaveasfilename()
+#file = asksaveasfile()
+message = 'HolaMundo 123456'            ##Mensaje a Cifrar
+print(message)
+print("Nombre del Archivo: ", f)
+
+################################################################ Cifrado AES ###################################################################
+
+
+ciphertext=''
+ciphertext = obj.encrypt(message)      ##Se almacena mensaje cifrado en variable ciphertext
+#print (ciphertext_bytes)
+
+
+############################################################### Descifrado AES ##################################################################
+
+print("############################ Descifrado ###############################")
+keyD= input("Introduce la llave (16 Caracteres): ")
+obj2 = AES.new(keyD, AES.MODE_CBC, iv)
+plaintext=obj2.decrypt(ciphertext)
+print (plaintext)
