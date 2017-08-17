b07632d6d1edf72f8bd79a65c9ba33d26af62488
ederAguirre <edjoo1113@gmail.com>
2017-08-17 01:26:45 -0500

    Modifcación "Lectura de Archivo"
    Sólo Cifra archivos .txt

--- TT-2016-B045/Codificacion/AES.py
+++ TT-2016-B045/Codificacion/AES.py
@@ -18,17 +18,18 @@ keyC= input("Introduce la llave (16 Caracteres): ")
 iv = Random.new().read(AES.block_size)   ##Creación de Vector de Inicialización
 obj = AES.new(keyC, AES.MODE_CBC, iv)    ##Creando Nuevo Objeto para cifrado AES
 #filename = askopenfilename()             # ##Dialogo para elegir archivo, y almacena ruta del archivo
-f = asksaveasfilename()
-#file = asksaveasfile()
-message = 'HolaMundo 123456'            ##Mensaje a Cifrar
-print(message)
-print("Nombre del Archivo: ", f)
+file = ''
+file = askopenfile("r")
+contenido = file.read()
+file.close()
+contenido_bytes= contenido.encode('utf-8')
+
 
 ################################################################ Cifrado AES ###################################################################
 
 
 ciphertext=''
-ciphertext = obj.encrypt(message)      ##Se almacena mensaje cifrado en variable ciphertext
+ciphertext = obj.encrypt(contenido_bytes)      ##Se almacena mensaje cifrado en variable ciphertext
 #print (ciphertext_bytes)
 
 
